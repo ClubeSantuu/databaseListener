fbb5e553f656796539daa086ef1b4694309cd08d
Danilo Nascimento <daconnas.dcn@gmail.com>
2023-04-11 11:05:31 -0300

    criando script para filtrar o schema das tabelas

---
+++ scripts/compare_db.py
@@ -0,0 +1,51 @@
+import argparse, json
+
+
+parser = argparse.ArgumentParser(prog="PROG")
+
+parser.add_argument("--pg", required=True)
+parser.add_argument("--msql", required=True)
+
+args = parser.parse_args()
+
+
+def filter(file) -> list:
+    res = []
+    get = False
+    name = ""
+    for line in file.readlines():
+        line:str = line.strip()
+
+        if get:
+            field = line.split(" ")[0].replace("`","")
+            if field not in ['PRIMARY', 'UNIQUE', 'KEY', 'CONSTRAINT', 'CONSTRAINT']:
+                table[name].append(field)
+
+        if line.upper().startswith("CREATE TABLE"):
+            get = True
+            name = line.split(" ")[2].replace("`","").split(".")[-1]
+            if name in [i for i in res]:
+                raise ValueError("no já existe")
+
+            table = {
+                name: []
+            }
+
+        if get and line.startswith(")") and line.endswith(";"):
+            get = False
+            del table[name][-1]
+            res.append(table)
+
+    return res
+
+
+def save(file_name: str):
+    with open(file_name) as file:
+        obj = filter(file)
+
+    with open(f"{file_name}.json", "w") as file:
+        json.dump(obj, file)
+
+
+save(args.pg)
+save(args.msql)
