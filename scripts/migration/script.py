e9ffe2c94ba331875502b554afece53d771e1c6a
Vinicius-C <vinicius_caina@live.com>
2023-04-16 17:35:13 -0300

    fix: scripts

--- scripts/migration/script.py
+++ scripts/migration/script.py
@@ -235,7 +235,7 @@ def convert_sql(sql):
         values = take_away_field(values, positions_to_remove)[0:-1]
         result = convert_values_in_insert(insert, values)
 
-        result = replace_string_between(result, "INTO \"" + table_name + "\" ", "VALUES", field_sequence + " ").replace("\"", "`")
+        result = replace_string_between(result, "INTO \"" + table_name.replace("\"", "`") + "\" ", "VALUES", field_sequence + " ")
         final_inserts.append(result)
     return "\n\n".join(final_inserts)
 
